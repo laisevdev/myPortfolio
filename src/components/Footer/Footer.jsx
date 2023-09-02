92bf4d7b14aed63badd9bf158a8b324e9b8b463d
Laíse Vieira <laisevdev@gmail.com>
2023-09-02 16:29:43 -0300

    add socialmedias picture

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,32 @@ const Footer = () => {
   return (
     <>
         <section className='footer'>
+          <h1>Entre em contato</h1>
+          <p>Estou sempre aberta para conversar, então não hesite em entrar em contato comigo</p>
+          
+          <ul className='chatme'>
+            <li>
+              <a href="">Diga Olá</a>
+            </li>            
+          </ul>
 
+          <div className='social'>
+          <ul>
+            <li>
+              <a href="">
+                <img src="" alt="" />
+              </a>
+            </li> 
+            <li>
+              <a href="">
+                <img src="" alt="" />
+              </a>
+            </li>          
+          </ul>
+          </div>
+          <h2>
+            ©Laíse Alves 2023
+          </h2>
         </section>
     </>
   )
