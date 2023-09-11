3dd2d1505b723a4bec623a17666ab19fd84b00c8
Laíse Vieira <laisevdev@gmail.com>
2023-09-11 16:21:46 -0300

    [feat]: add socialmedia links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,12 +26,12 @@ const Footer = () => {
           </h2>
           <ul>
             <li>
-              <a href=""  target='blank_'> 
+              <a href="https://www.linkedin.com/in/laisevdev/"  target='blank_'> 
                 <img src={LINKEDIN}alt="LINKEDIN" />
               </a>
             </li> 
             <li>
-              <a href=""  target='blank_'>
+              <a href="https://github.com/laisevdev"  target='blank_'>
                 <img src={GITHUB} alt="GITHUB" />
               </a>
             </li>          
