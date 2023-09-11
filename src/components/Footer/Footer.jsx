911d78cd2a2d89edb967f52c2a076dce7f2db146
Laíse Vieira <laisevdev@gmail.com>
2023-09-11 16:52:03 -0300

    [refactor]: add button telegram link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,10 @@ import imggithub from '../../Assets/giticon.png'
 
 const LINKEDIN = imglinkedin;
 const GITHUB = imggithub;
+const TELEGRAM_LINK = 'https://t.me/laisev';
+const LINKEDIN_LINK = 'https://www.linkedin.com/in/laisevdev/';
+const GITHUB_LINK = 'https://github.com/laisevdev';
+
 
 const Footer = () => {
   return (
@@ -14,11 +18,11 @@ const Footer = () => {
             <p>Estou sempre aberta para conversar, então não hesite em entrar em contato comigo.</p>
           </div>
         
-          <ul className='chatme'>
-            <li>
-              <a href="https://t.me/laisev" target='blank_'>Diga Olá</a>
-            </li>            
-          </ul>
+          <div className='chatme'>
+              <a href={TELEGRAM_LINK} target='blank_' rel="noopener noreferrer">
+                <button>Diga Olá</button>
+              </a>                     
+          </div>
 
           <div className='social'>
           <h2>
@@ -26,12 +30,12 @@ const Footer = () => {
           </h2>
           <ul>
             <li>
-              <a href="https://www.linkedin.com/in/laisevdev/"  target='blank_'> 
+              <a href={LINKEDIN_LINK}  target='blank_'> 
                 <img src={LINKEDIN}alt="LINKEDIN" />
               </a>
             </li> 
             <li>
-              <a href="https://github.com/laisevdev"  target='blank_'>
+              <a href={GITHUB_LINK}  target='blank_'>
                 <img src={GITHUB} alt="GITHUB" />
               </a>
             </li>          
