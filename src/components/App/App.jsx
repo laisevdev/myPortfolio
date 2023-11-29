e13dbc92312832559cf0336496b27d00dd9cdfb6
Laíse Vieira <laisevdev@gmail.com>
2023-11-29 12:18:53 -0300

    [refactor]: format document

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,21 @@
-import './App.css';
-import Navbar from '../Navbar/Navbar';
-import Main from '../Main/Main';
-import Section from '../Section/Section'
-import Projects from '../Projects/Project';
-import Footer from '../Footer/Footer';
-import { motion } from "framer-motion"
+import "./App.css";
+import Navbar from "../Navbar/Navbar";
+import Main from "../Main/Main";
+import Section from "../Section/Section";
+import Projects from "../Projects/Project";
+import Footer from "../Footer/Footer";
 
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ ease: "easeInOut", duration: 1.1, delay: 0.3 }}
-          className='framer-motion'
-        >
-            <div className='framer-motion'>
-              <Main />
-              <Section />
-              <Projects />
-            </div>
-          <Footer /> 
-        </motion.div>       
+      <Navbar />
+      <Main />
+      <Section />
+      <Projects />
+      <Footer />
     </>
-    
   );
 };
 
-export default App
+export default App;
