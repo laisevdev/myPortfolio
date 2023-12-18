b6b6533c99b1bc2cd028ae42700040b7a9dc83ae
Laíse Vieira <laisevdev@gmail.com>
2023-12-18 10:26:03 -0300

    [feat]: add checkbox menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,43 @@
-import './Navbar.css'
-import imglogo from '../../Assets/Logotipo.jpg'
-import './Responsive.css'
-
+import "./Navbar.css";
+import imglogo from "../../Assets/Logotipo.jpg";
+import "./Responsive.css";
 
 const logo = imglogo;
 
 const Navbar = () => {
   return (
-  <>
-    <nav className='navbar'>
-      <div className='logotipo'>
-          <img className='image' src={logo} alt='Logotipo'/>      
-      </div>  
+    <>
+      <nav className="navbar">
+        <div className="logotipo">
+          <img className="image" src={logo} alt="Logotipo" />
+        </div>
+
+        <label className="checkbox-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
 
         <ul className="menu">
           <li>
-            <a href="#sobremim">
-              Inicio
-            </a>
+            <a href="#sobremim">Inicio</a>
           </li>
           <li>
-            <a href="#sobremim">
-              Sobre Mim
-            </a>
+            <a href="#sobremim">Sobre Mim</a>
           </li>
           <li>
-            <a href="#projetos">Projetos
-            </a>
+            <a href="#projetos">Projetos</a>
           </li>
 
           <li>
-            <a href="#formacao">Formação
-            </a>
+            <a href="#formacao">Formação</a>
           </li>
-          <li className='contato'>
-            <a href="#contato">
-              Contato
-            </a>
+          <li className="contato">
+            <a href="#contato">Contato</a>
           </li>
         </ul>
-    </nav>
-  </>
+      </nav>
+    </>
   );
 };
 
