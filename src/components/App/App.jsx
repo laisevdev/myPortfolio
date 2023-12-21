f295b825ac1945f6828c9add13e0f0b50fdbfd22
Laíse Vieira <laisevdev@gmail.com>
2023-12-21 01:56:39 -0300

    [feat]: add Menu Mobile component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,11 +4,15 @@ import Main from "../Main/Main";
 import Section from "../Section/Section";
 import Projects from "../Projects/Project";
 import Footer from "../Footer/Footer";
+import MenuMobile from "../MobileMenu/Menu";
 
 
 const App = () => {
   return (
     <>
+      <div className="style-mobile">
+      <MenuMobile />
+      </div>
       <Navbar />
       <Main />
       <Section />
