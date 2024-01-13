784ad343ff80f6ba42d17f111c1a96facc7708f9
eduvarjaor <eduardovarjaor@gmail.com>
2024-01-13 19:00:48 -0300

    fix(Main): adicionado id inicio

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,51 +1,61 @@
-import './Main.css'
-import imgprof from '../../Assets/profile.jpeg'
-import curriculo from '../../Assets/desenvblockchain.pdf'
+import "./Main.css";
+import imgprof from "../../Assets/profile.jpeg";
+import curriculo from "../../Assets/desenvblockchain.pdf";
 
 import { motion } from "framer-motion";
-import './Responsive.css'
+import "./Responsive.css";
 
 const PICTURE = imgprof;
 const COVERLETTER = curriculo;
 
 const Main = () => {
+    const handleDownload = () => {
+        const PDFFILEPATH = COVERLETTER;
 
-  const handleDownload = () => {
-   
-    const PDFFILEPATH = COVERLETTER;
-
-    const link = document.createElement('a');
-    link.href = PDFFILEPATH;
-    link.download = 'desenvblockchain.pdf';
-    link.click();
-  };
+        const link = document.createElement("a");
+        link.href = PDFFILEPATH;
+        link.download = "desenvblockchain.pdf";
+        link.click();
+    };
 
     return (
-      <> 
-       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ ease: "easeInOut", duration: 1.0, delay: 0.3 }}
-      >
-        
-        <main className='main'>
-          <div className='profile'>    
-            <h1 className='title'>Olá, Bem-vindo(a)!🖐Sou <span className='myname'>Laíse Vieira Alves</span></h1>
-             
-            <h2>Desenvolvedora <span className='block'> Blockchain</span></h2> 
+        <div id="inicio">
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ ease: "easeInOut", duration: 1.0, delay: 0.3 }}
+            >
+                <main className="main">
+                    <div className="profile">
+                        <h1 className="title">
+                            Olá, Bem-vindo(a)!🖐Sou{" "}
+                            <span className="myname">Laíse Vieira Alves</span>
+                        </h1>
 
-            <p>Apaixonada por tecnologia, principalmente aquelas que <br /> envolvem o mercado de Blockchain, como: DEFI, NFT, <br /> DAOS, Cryptos e etc.</p>
+                        <h2>
+                            Desenvolvedora{" "}
+                            <span className="block"> Blockchain</span>
+                        </h2>
 
-            <button onClick={handleDownload}> &#129139; Download CV</button>
-          </div>
+                        <p>
+                            Apaixonada por tecnologia, principalmente aquelas
+                            que <br /> envolvem o mercado de Blockchain, como:
+                            DEFI, NFT, <br /> DAOS, Cryptos e etc.
+                        </p>
 
-          <div className='profile-img'>
-            <img src={PICTURE} alt="profile" />
-          </div>
-        </main>    
-      </motion.div>  
-      </>  
-    )
-  }
-  
-  export default Main
+                        <button onClick={handleDownload}>
+                            {" "}
+                            &#129139; Download CV
+                        </button>
+                    </div>
+
+                    <div className="profile-img">
+                        <img src={PICTURE} alt="profile" />
+                    </div>
+                </main>
+            </motion.div>
+        </div>
+    );
+};
+
+export default Main;
