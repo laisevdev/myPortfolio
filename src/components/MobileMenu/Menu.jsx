2deeaaa8c2fd7cd4040627cac8ebc4857674d339
Laíse Vieira <laisevdev@gmail.com>
2024-05-31 23:46:51 -0300

    [refactor]: menu mobile

--- src/components/MobileMenu/Menu.jsx
+++ src/components/MobileMenu/Menu.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { TiThMenu } from "react-icons/ti";
 import { IoCloseSharp } from "react-icons/io5";
 import './Menu.css';
 
 const MenuMobile = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -16,11 +17,19 @@ const MenuMobile = () => {
     }
   };
 
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuOpen(false);
+    }
+  };
+
   useEffect(() => {
     window.addEventListener("resize", handleResize);
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
       window.removeEventListener("resize", handleResize);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [menuOpen]);
 
@@ -30,16 +39,19 @@ const MenuMobile = () => {
         {menuOpen ? <IoCloseSharp color="white" size="26px"/> : <TiThMenu color="white" size="35px"/>}
       </div>
       {menuOpen && (
-        <div className="mobile-menu">
+        <div className="mobile-menu" ref={menuRef}>
           <div className="close-mobile" onClick={toggleMenu}>
             <IoCloseSharp color="white" size="26px"/>
           </div>
-          <ul onClick={toggleMenu}>
+          <ul>
             <li>
               <a href="#sobremim" onClick={toggleMenu}>Sobre Mim</a>
             </li>
             <li>
               <a href="#projetos" onClick={toggleMenu}>Projetos</a>
+            </li>
+            <li>
+              <a href="#formacao" onClick={toggleMenu}>Cursos</a>
             </li>
             <li className="contato">
               <a href="#contato" onClick={toggleMenu}>Contato</a>
@@ -52,4 +64,3 @@ const MenuMobile = () => {
 };
 
 export default MenuMobile;
-
